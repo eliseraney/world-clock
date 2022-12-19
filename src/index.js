10f2df3944fcacee5d1f75ef5027b224148242f2
Elise <eliseraney@gmail.com>
2022-12-19 16:43:28 -0700

    Added JS to date & time elements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
+function displayTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  newYorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let londonElement = document.querySelector("#london");
+  let londonTimeElement = londonElement.querySelector(".time");
+  londonTimeElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  tokyoTimeElement.innerHTML = moment()
+    .tz("Asia/Tokyo")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  sydneyTimeElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("hh:mm:ss [<small>]A[</small>]");
+}
+
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 losAngelesDateElement.innerHTML = moment()
-  .tz("America/Los Angeles")
+  .tz("America/Los_Angeles")
   .format("MMMM D, YYYY");
 
 let newYorkElement = document.querySelector("#new-york");
 let newYorkDateElement = newYorkElement.querySelector(".date");
 newYorkDateElement.innerHTML = moment()
-  .tz("America/New York")
+  .tz("America/New_York")
   .format("MMMM D, YYYY");
 
 let londonElement = document.querySelector("#london");
@@ -25,3 +57,6 @@ let sydneyDateElement = sydneyElement.querySelector(".date");
 sydneyDateElement.innerHTML = moment()
   .tz("Australia/Sydney")
   .format("MMMM D, YYYY");
+
+displayTime();
+setInterval(displayTime, 1000);
